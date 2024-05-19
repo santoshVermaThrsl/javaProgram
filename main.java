6c86e9ee0cc277453f98c63ab15d1dde2c9e92d0
santoshVermaThrsl <santosh.kumar@hitechroboticsystemz.com>
2024-05-19 16:27:36 +0530

    second commit

--- main.java
+++ main.java
@@ -2,5 +2,7 @@ public class main{
 public static void main(String [] args){
      System.out.println("This is santosh kumar verma");
      System.out.println("This is my first project in jenkins");
+     System.out.println("i am adding one more lines");
+     System.out.println("WOOOOOOW");
 }
 }
